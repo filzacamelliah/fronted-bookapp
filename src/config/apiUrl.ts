9461c7fd9622a2733b8a320f26e66b15b848febe
filzacamellia <filzacamelliah@gmail.com>
2024-06-02 17:20:04 +0700

    tambahan

--- src/config/apiUrl.ts
+++ src/config/apiUrl.ts
@@ -1,4 +1,4 @@
 export const API_URL =
   process.env.NODE_ENV === "production"
-    ? "http://localhost:8000"
-    : "http://localhost:8000";
+    ? "http://103.74.5.211:8000"
+    : "http://103.74.5.211:8000";
